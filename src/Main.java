78c5a47ae562efa67a509c6da97cee3308fa98e1
kroka6 <krockovamartina@gmail.com>
2023-07-27 15:37:30 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(count(3, 5));
+    }
+
+    public static int count(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/MainTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    Main main;
+
+    @Test
+    void test_main() {
+
+        int result1 = Main.count(3, 5);
+        assertEquals(8, result1);
+
+        int result2 = Main.count(0, 0);
+        assertEquals(0, result2);
+
+
+        int result3 = Main.count(-2, 7);
+        assertEquals(5, result3);
+    }
+}
